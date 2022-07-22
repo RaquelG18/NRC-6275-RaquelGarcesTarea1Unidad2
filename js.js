e7611e342252fbdd803127d3abfb255802eebff8
RaquelG18 <orgarces@espe.edu.ec>
2022-07-21 20:20:31 -0500

    update

--- js.js
+++ js.js
@@ -1,4 +1,14 @@
+/*
+    Creamos la función para la validación de 
+    los campos de nuestro formulario
+*/
 $(document).ready(function () {
+    /* como parámetro añadimos el id de nuestro formulario
+        #datosformulario 
+         Llamamos al método validate() y agregamos 
+         como parámetro los campos del formulario 
+         con la restricción de tipo required
+        */
     $("#datosformulario").validate({
         rules: {
             name: {
